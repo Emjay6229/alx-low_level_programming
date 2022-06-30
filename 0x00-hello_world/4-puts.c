1e55adc965294190ec5fa027d5a584e6867aeeb5
Emjay6299 <emjay6299@gmail.com>
2022-06-30 09:44:10 -0700

    C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block to execute
+ * Return - 0 for int
+ */
+int main(void)
+{
+puts("\"Programming is kike building a multilingual puzzle"
+);
+return (0)
+}
