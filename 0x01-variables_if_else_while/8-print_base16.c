ba411b5c3e8c2682a1f5aa2e525f9a69d344b32a
Emjay6299 <emjay6299@gmail.com>
2022-07-02 02:23:18 -0700

    prints numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block to be executed
+ * Description: prints numbers of base 16
+ * Return: 0 success
+ */
+int main(void)
+{
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0)
+}
